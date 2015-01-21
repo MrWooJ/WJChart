85d2e390298c8f3baa2ee15ac5e225a60d1a6c8d
MrWooJ <hurricanc@gmail.com>
2015-01-21 09:39:01 +0330

    Adding XCode Project

---
+++ WJChart/WJChart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WJChart
+//
+//  Created by Alireza Arabi on 1/21/15.
+//  Copyright (c) 2015 Alireza Arabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WJChart/WJChart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WJChart
+//
+//  Created by Alireza Arabi on 1/21/15.
+//  Copyright (c) 2015 Alireza Arabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
